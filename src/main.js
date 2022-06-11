bcfe34885af0ff845376be50f2295610aeeeff41
Tene <tene@nextunicorn.kr>
2022-06-11 17:55:41 +0900

    ✒️Feat: 기능 구현

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import '@picocss/pico';
+
+const $sub = document.getElementById('sub');
+const $reset = document.getElementById('reset');
+const $add = document.getElementById('add');
+
+const sum = (counter, operand) => counter + operand;
+
+const handleClickButton = ({ target }) => {
+  const $counter = document.getElementById('counter');
+  const nextCounter = target.textContent === 'RESET' ? 0 : sum(+$counter.textContent, +target.textContent);
+
+  $counter.textContent = nextCounter;
+};
+
+[$sub, $reset, $add].forEach(button => button.addEventListener('click', handleClickButton));
